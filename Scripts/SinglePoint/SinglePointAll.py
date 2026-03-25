6ebd7700336680e7debba4365d7654990fb055c3
Alexandre Dufresne Nappert <93613553+MrDNAlex@users.noreply.github.com>
2026-03-25 12:16:46 -0400

    Created a Script for all Single Point
    - Created a script to run all Single Point Calculation
    - Tested the Script

---
+++ Scripts/SinglePoint/SinglePointAll.py
@@ -0,0 +1,14 @@
+from SinglePointHydrogen import runSinglePoint as runSinglePointHydrogen
+from SinglePointCarbon import runSinglePoint as runSinglePointCarbon
+from SinglePointNitrogen import runSinglePoint as runSinglePointNitrogen
+from SinglePointOxygen import runSinglePoint as runSinglePointOxygen
+from SinglePointFluorine import runSinglePoint as runSinglePointFluorine
+from SinglePointChlorine import runSinglePoint as runSinglePointChlorine
+
+# Run All Single Point Calculations
+runSinglePointHydrogen()
+runSinglePointCarbon()
+runSinglePointNitrogen()
+runSinglePointOxygen()
+runSinglePointFluorine()
+runSinglePointChlorine()
