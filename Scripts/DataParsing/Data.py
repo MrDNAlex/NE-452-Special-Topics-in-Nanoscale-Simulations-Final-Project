b979fe9dd675946fcb2ef35b16d4c006caee7048
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2026-03-31 16:13:57 -0400

    Created a common Data File
    - Created a file with the common data that are needed for the parsing steps

---
+++ Scripts/DataParsing/Data.py
@@ -0,0 +1,46 @@
+# Define the functionals
+FUNCTIONALS = ["LDA", "PBE", "B3LYP", "B97X", "M062X"]
+
+# Define the Molecules
+MOLECULES = [
+    "Methane",
+    "Ethane",
+    "Propane",
+    "Butane",
+    "Pentane",
+    "Hexane",
+    "Heptane",
+    "Octane",
+    "Nonane",
+    "Aspirin",
+    "Azobenzene",
+    "Caffeine",
+    "Ethanol",
+    "Pyrrole",
+    "Acetaminophen",
+    "Diclofenac",
+    "Ibuprofen",
+    "Hydrocortisone",
+    "Dexamethasone",
+    "Prednisone"
+]
+
+# Define the Common Atoms
+ATOMS = [
+    "Hydrogen",
+    "Carbon",
+    "Nitrogen",
+    "Oxygen",
+    "Fluorine",
+    "Chlorine"
+]
+
+# Define the Common Atoms
+ATOMS_ELEMENTS = [
+    "H",
+    "C",
+    "N",
+    "O",
+    "F",
+    "Cl"
+]
