21d93223f52801df7579540e8a56a524e044e639
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2026-03-31 20:32:58 -0400

    Created a script to calculate the final enthalpy
    - Created a script to calculate the final Enthalpy

---
+++ Scripts/DataParsing/CalculateFinalEnthalpy.py
@@ -0,0 +1,43 @@
+from Data import FUNCTIONALS, MOLECULES, ATOMS
+import pandas as pd
+
+# Load the Reference DataFrames
+enthalpyDataFrame = pd.read_csv("Results/MoleculeEnthalpies.csv")
+xyzDataFrame = pd.read_csv("Results/AtomCount.csv")
+atomEnthalpiesDataFrame = pd.read_csv("Results/AtomEnthalpies.csv")
+
+masterDataFrame = enthalpyDataFrame.copy()
+
+for i in range(1, len(xyzDataFrame.columns)):
+    
+    columnName = xyzDataFrame.columns[i]
+    atomName = columnName.split()[0]
+    enthalpyCorrectionColumnName = f"{atomName} Enthalpy Correction" 
+    masterDataFrame[columnName] = xyzDataFrame[columnName]
+    
+    masterDataFrame[enthalpyCorrectionColumnName] = masterDataFrame[columnName] * atomEnthalpiesDataFrame["Experimental Enthalpy Correction"][ATOMS[atomName]]
+    
+    for func in FUNCTIONALS:
+        enthalpyColumnName = f"{atomName} {func} Enthalpy" 
+        functionalEnthalpies = atomEnthalpiesDataFrame[f"{func} Enthalpy"]
+        
+        masterDataFrame[enthalpyColumnName] = masterDataFrame[columnName] * functionalEnthalpies[ATOMS[atomName]]
+        
+print(masterDataFrame)
+
+for i in range(len(FUNCTIONALS)):
+#for func in FUNCTIONALS:
+    func = FUNCTIONALS[i]
+    columnName = f"Final {func} Enthalpy"
+    
+    elementSums = [0 for _ in range(len(MOLECULES))]
+    correctionSums = [0 for _ in range(len(MOLECULES))]
+    
+    for atom in ATOMS.keys():
+        elementSums += masterDataFrame[f"{atom} {func} Enthalpy"]
+        correctionSums += masterDataFrame[f"{atom} Enthalpy Correction"]
+    
+    masterDataFrame[columnName] = masterDataFrame[masterDataFrame.columns[1 + i]] - elementSums + correctionSums
+        
+print(masterDataFrame)
+masterDataFrame.to_csv("Results/MasterEnthalpies.csv", index=False)
