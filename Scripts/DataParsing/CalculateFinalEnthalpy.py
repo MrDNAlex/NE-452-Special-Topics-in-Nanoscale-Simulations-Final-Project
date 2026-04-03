8fb735c5b5e83e76252dbcf869fad0e5335ca6c1
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2026-04-03 01:35:13 -0400

    Added comments and fixed columns names
    - Added comments to better explain the file
    - Propogated column name changes to the file
    - Fixed the math accounting for the sum in the single point file

--- Scripts/DataParsing/CalculateFinalEnthalpy.py
+++ Scripts/DataParsing/CalculateFinalEnthalpy.py
@@ -9,34 +9,36 @@ def Parse():
 
     masterDataFrame = enthalpyDataFrame.copy()
 
+    # Add a Column for the Number of certain Atoms in each molecule
+    # 
+    # Multiply the Atomic Enthalpies by the number of Atoms contained in the molecule
     for i in range(1, len(xyzDataFrame.columns)):
-        
         columnName = xyzDataFrame.columns[i]
         atomName = columnName.split()[0]
         enthalpyCorrectionColumnName = f"{atomName} Enthalpy Correction" 
+        
+        # Add number of Atoms per molecule of certain type
         masterDataFrame[columnName] = xyzDataFrame[columnName]
         
+        # Add the experimental Correction Enthalpy
         masterDataFrame[enthalpyCorrectionColumnName] = masterDataFrame[columnName] * atomEnthalpiesDataFrame["Experimental Enthalpy Correction"][ATOMS[atomName]]
         
+        # Calaculate the total enthalpy contribution for a certain atom
         for func in FUNCTIONALS:
-            enthalpyColumnName = f"{atomName} {func} Enthalpy" 
-            functionalEnthalpies = atomEnthalpiesDataFrame[f"{func} Enthalpy"]
-            
-            masterDataFrame[enthalpyColumnName] = masterDataFrame[columnName] * functionalEnthalpies[ATOMS[atomName]]
-            
+            enthalpyColumnName = f"Final {atomName} {func} Enthalpy"
+            functionalEnthalpy = atomEnthalpiesDataFrame[f"Final Atom {func} Enthalpy"] 
+    
+            masterDataFrame[enthalpyColumnName] = masterDataFrame[columnName] * functionalEnthalpy[ATOMS[atomName]]
+
+    # Subtract Sum of the Total Enthalpy Contribution of each Atom from the Total Molecule Enthalpy to get Formation
     for i in range(len(FUNCTIONALS)):
-    #for func in FUNCTIONALS:
         func = FUNCTIONALS[i]
-        columnName = f"Final {func} Enthalpy"
-        
         elementSums = [0 for _ in range(len(MOLECULES))]
-        correctionSums = [0 for _ in range(len(MOLECULES))]
         
         for atom in ATOMS.keys():
-            elementSums += masterDataFrame[f"{atom} {func} Enthalpy"]
-            correctionSums += masterDataFrame[f"{atom} Enthalpy Correction"]
+            elementSums += masterDataFrame[f"Final {atom} {func} Enthalpy"]
         
-        masterDataFrame[columnName] = masterDataFrame[masterDataFrame.columns[1 + i]] - elementSums + correctionSums
+        masterDataFrame[f"{func} Enthalpy of Formation"] = masterDataFrame[masterDataFrame.columns[1 + i]] - elementSums
             
     print(masterDataFrame)
     masterDataFrame.to_csv("Results/MasterEnthalpies.csv", index=False)
